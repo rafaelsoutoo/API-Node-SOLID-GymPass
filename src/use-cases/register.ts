9dc4503afc1844e412aaac708e9c88bc24751a13
rafaelsoutoo <rafaelsouto70017@gmail.com>
2024-05-09 22:52:56 -0300

    5-7

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,7 +1,7 @@
-import { UsersRepository } from "@/repositories/users-repositorey";
-import { hash } from "bcryptjs";
-import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
-import { User } from "@prisma/client";
+import { UsersRepository } from "@/repositories/users-repositorey"
+import { hash } from "bcryptjs"
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error"
+import { User } from "@prisma/client"
 
 interface RegisterUseCaseRequest {
   //para criar uma user precisamos..
