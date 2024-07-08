b8ebf5ac9851bd36d8cf41eee538d3173bb805c2
rafaelsoutooclear <rafaelsouto70017@gmail.com>
2024-07-08 13:25:19 -0300

    create prisma repository check ins, 6-1

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -3,7 +3,7 @@ import { CheckIn, Prisma } from "@prisma/client";
 export interface CheckInsRepository {
     create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>; // utilizado o Unchecked quando preciso fazer uma relação cocom os componentes ja exixtentes
     save(checkIn: CheckIn): Promise<CheckIn>; 
-    findByUserIdOnDate(userId: string, data: Date): Promise<CheckIn | null>
+    findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
     findById(id: string): Promise<CheckIn | null>
     findManyByUserId(userId: string, page: number): Promise <CheckIn[]>
     countByUserId(userId: string): Promise<number>
--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,14 +1,18 @@
 import { prisma } from "@/lib/prisma";
-import { Prisma, User } from "@prisma/client";
+import { Prisma } from "@prisma/client";
 import { UsersRepository } from "../users-repositorey";
 
 export class PrismaUsersRepository implements UsersRepository {
-  
-  findById(id: string): Promise<User| null> {
-    throw new Error ('jkbasjdkbajsb')
-    
+
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+    return user
   }
-  
+
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
@@ -18,7 +22,7 @@ export class PrismaUsersRepository implements UsersRepository {
 
     return user
   } //todos operações do database sempre pasaram nos repositorios
-  
+
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
       data,
