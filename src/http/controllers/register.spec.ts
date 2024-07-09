2b99da8842e8e150cfb4659c257fed8dbc1c320b
rafaelsoutooclear <rafaelsouto70017@gmail.com>
2024-07-09 20:40:23 -0300

    create test register e2e, 6-10

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,28 @@
-import { test } from "vitest";
+import request from 'supertest'
+import { app } from '@/app'
+import { describe } from 'node:test'
+import { afterAll, beforeAll, expect, it } from 'vitest'
 
-test('ok', () => {})
+
+describe('Register (e2e', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+
+    afterAll(async ()=> {
+        await app.close()
+    })
+
+
+    it('should be able to register', async () => {
+        const response = await request(app.server)
+            .post('/users')
+            .send({
+                name:'John Doe',
+                email: 'john@gmail.com',
+                password: '123456'
+            })
+
+            expect(response.statusCode).toEqual(201)
+    })
+})
