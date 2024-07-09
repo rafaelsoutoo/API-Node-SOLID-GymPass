fc48f04961829b7f966bd874be4ea2f7bb210779
rafaelsoutooclear <rafaelsouto70017@gmail.com>
2024-07-09 19:52:17 -0300

    create test e2e, environment, 6-7

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,9 @@ import tsconfigPaths from "vitest-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    environmentMatchGlobs: [
+      ['src/http/controllers/**', 'prisma'] // todos test que estiverem na pasta controller eu quero que use o environment novo 
+    ]
+  }
 });
