ddf14211819760628eeefd2b42522c59a2a6ba5a
rafelsoutoo <rafaelsouto70017@gmail.com>
2024-07-18 23:38:58 -0300

    initial refresh token. 7-1

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { fastify } from "fastify";
 import { ZodError } from "zod";
 import { env } from "./env";
 import fastifyJwt from "@fastify/jwt";
+import fastifyCookie  from "@fastify/cookie";
 import { gymsRoutes } from "./http/controllers/gyms/routes";
 import { usersRoutes } from "./http/controllers/users/routes";
 import { checkInsRoutes } from "./http/controllers/check-ins/routes";
@@ -10,9 +11,17 @@ export const app = fastify();
 
 
 app.register(fastifyJwt, {
-    secret: env.JWT_SECRET
+    secret: env.JWT_SECRET,
+    cookie: {
+        cookieName: 'refreshToken',
+        signed: false // n é um cookie assinado
+    },
+    sign: {
+        expiresIn: '10m' 
+    }
 })
 
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 app.register(gymsRoutes)
--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -27,10 +27,27 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
       }
     }) // serve para criar um novo token
 
-    return reply.status(200).send({
-      token
-    });
-    
+    const refreshToken = await reply.jwtSign({}, {
+      sign: {
+        sub: user.id,
+        expiresIn: '7d', //perderá a auth se ficar 7 dias sem entrar, pois a cada 10m é atualizado o jwt
+      }
+    })
+
+    return reply
+      .setCookie('refreshToken', refreshToken,{
+        path: '/', // quaais rotas tem acesso, no caso de / todas
+        secure: true, // o cookie será encriptado através de HTTPs
+        sameSite: true, // esse cookie só vai ser acessivél dentro do mesmo domínio
+        httpOnly: true //apenas acessado pelo backEnd
+
+
+      })
+      .status(200)
+      .send({
+        token
+      });
+
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: error.message });
